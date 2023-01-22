3f0c75400b8cf855d867845d923cb985c9cf86a5
alu0101339542 <alu0101339542@ull.edu.es>
2023-01-22 21:00:46 +0000

    seguimos coveralls

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,14 +1,7 @@
 const request = require('supertest')
 const app = require('../app');
 const mongoose = require("mongoose");
-const express = require('express');
-const router = express.Router();
-const User = require('../models/user');
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const config = require('../dbconfig/database');
-const http = require("https");
-const { json } = require('body-parser');
+
 
 mongoose.connect('mongodb://equipo12:equipo12@172.16.130.61:27017/Proyecto', {
   useNewUrlParser: true,
--- app.js
+++ app.js
@@ -60,6 +60,6 @@ var server = app.listen(port, ip.address(), function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  //console.log('Example app listening at http://%s:%s', host, port)
 
 });
